b993261504d92e6e8be5ee05ce8e173bc0b58b4f
Purushotam Shrestha <puspedro@gmail.com>
2016-12-14 10:33:11 +0000

    model

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -7,7 +7,25 @@ class IdeasController < ApplicationController
     @idea = Idea.create(idea_params)
     redirect_to root_path
     end
+    
+    def edit
+        @idea = Idea.find(params[:id])
+    end
+    
+    def update
+        @idea = Idea.find(params[:id])
+        if @idea.update(idea_params)
+            redirect_to root_path
+        else
+            redirect_to edit_idea_path(params[:id])
+        end
+    end
 
+    def destroy
+        @idea = Idea.find(params[:id])
+        @idea.destroy
+        redirect_to root_path
+    end
 private
 
   def idea_params
